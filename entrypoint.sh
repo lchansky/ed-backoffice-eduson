86c38139daeaf92be453dca64d8cf2a762f8836f
lchansky <lchansky@mail.ru>
2023-08-03 15:49:59 +0300

    Убрал таймаут и healthcheck урлов

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py init_courses  # Кастомная команда
 
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn proj.wsgi:application --bind 0.0.0.0:5000 --timeout 1000
+gunicorn proj.wsgi:application --bind 0.0.0.0:5000
