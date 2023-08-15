d2fdc8926d01771a1b12b3c53bff2be8b01baba1
lchansky <lchansky@mail.ru>
2023-08-15 11:44:54 +0300

    Добавил фильтр на промокоды: Все / Активные / Не активные

--- src/promocodes/views.py
+++ src/promocodes/views.py
@@ -18,3 +18,23 @@ class PromocodeList(LoginRequiredMixin, ListView):
     login_url = 'login'
     paginate_by = 10
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        is_active = self.request.GET.get('is_active')
+        if is_active:
+            context['active_filters_query'] = f'is_active={is_active}'
+        else:
+            context['active_filters_query'] = ''
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        is_active = self.request.GET.get('is_active')
+
+        if is_active:
+            is_active = True if is_active == 'true' else False
+            queryset = queryset.filter(is_active=is_active)
+
+        return queryset
+
