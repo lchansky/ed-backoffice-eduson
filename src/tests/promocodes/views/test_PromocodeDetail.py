3a18a87538240466e6f8f9e4df8cfd9cce0a4c65
lchansky <lchansky@mail.ru>
2023-08-23 10:56:22 +0300

    Тест на promocode_detail

---
+++ src/tests/promocodes/views/test_PromocodeDetail.py
@@ -0,0 +1,35 @@
+from urllib.parse import urlencode
+
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+from tests.conftest import user
+from promocodes.models import Promocode
+
+
+@pytest.mark.django_db
+def test_show_promocode(user):
+    c = Client()
+    assert c.login(username='user', password='password')
+    promocode = Promocode.objects.create(name='PROMOCODE', type='additional_discount')
+    promocode_detail_endpoint = reverse("promocode_detail", args=(promocode.pk,))
+
+    response = c.get(promocode_detail_endpoint)
+
+    assert response.status_code == 200
+    assert response.context['promocode'] == promocode
+    assert response.template_name == ['promocodes/promocode_detail.html']
+
+
+@pytest.mark.django_db
+def test_show_promocode_without_login(user):
+    c = Client()
+
+    promocode = Promocode.objects.create(name='PROMOCODE', type='additional_discount')
+    promocode_detail_endpoint = reverse("promocode_detail", args=(promocode.pk,))
+
+    response = c.get(promocode_detail_endpoint)
+
+    assert response.status_code == 302
+    assert response.url == reverse('login') + f'?next={promocode_detail_endpoint}'
